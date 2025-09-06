2b824f77fc3d04bcd62f5a7fc695b1dab1d7d42b
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-09-06 17:47:36 +0530

    added type=>module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import jsonwebtoken from 'jsonwebtoken';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.post("/api/v1/signup", (req, res) => {
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+})
+
+app.post("/api/v1/signin", (req, res) => {
+    
+})
+
+app.post("/api/v1/content", (req, res) => {
+    
+})
+
+app.get("/api/v1/content", (req, res) => {
+    
+})
+
+app.delete("/api/v1/content", (req, res) => {
+    
+})
+
+app.post("/api/v1/brain/share", (req, res) => {
+    
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+    
+})
