1f9acc06cf0d6aec936ba5b4e7213b22fe4d44f7
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-10-06 17:21:03 +0530

    all files removed

--- src/db.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { model, Schema } from "mongoose";
-
-
-const UserSchema = new Schema({
-    username: {type: String, unique: true},
-    password: String
-})
-
-export const UserModel = model("User", UserSchema);
